3761f073b18334cce38b8585ce3e812c7efd341d
Theodore <unawares15@gmail.com>
2022-02-18 10:19:02 +0600

    fix vnpay urls

--- ecommerce/megauni/payment/vnpay/urls.py
+++ ecommerce/megauni/payment/vnpay/urls.py
@@ -6,11 +6,7 @@ from django.conf.urls import include, url
 from ecommerce.megauni.payment.vnpay import views as vnpay
 
 
-VNPAY_URLS = [
-    url(r'^ipn/$', vnpay.VNPayIPNView.as_view(), name='vnpay-ipn'),
-    url(r'^return/$', vnpay.VNPayReturnView.as_view(), name='vnpay-return'),
-]
-
 urlpatterns = [
-    url(r'^vnpay/', include((VNPAY_URLS, 'vnpay'))),
+    url(r'^ipn/$', vnpay.VNPayIPNView.as_view(), name='ipn'),
+    url(r'^return/$', vnpay.VNPayReturnView.as_view(), name='return'),
 ]
